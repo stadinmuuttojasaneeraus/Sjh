9217b1f934ab9d524b046e9336fa7007a38210f1
stadinmuuttojasaneeraus <remestor13@gmail.com>
2026-03-28 14:37:25 +0200

    Update menu.js

--- scripts /menu.js
+++ scripts /menu.js
@@ -1,6 +1,16 @@
 const menuIcon = document.getElementById("menu-icon");
 const navMenu = document.getElementById("nav-menu");
+const accordionBtns = document.querySelectorAll(".accordion-btn");
 
+// Öppna/stäng mobilmenyn
 menuIcon.addEventListener("click", () => {
   navMenu.classList.toggle("open");
+});
+
+// Accordion funktion för undermenyer på mobil
+accordionBtns.forEach(btn => {
+  btn.addEventListener("click", () => {
+    const submenu = btn.nextElementSibling;
+    submenu.classList.toggle("open");
+  });
 });
